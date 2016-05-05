489023ae90defe6710bb0cdb2b535a59d0e583ce
yichengjie <626659321@qq.com>
2016-05-05 21:21:31 +0800

    commit

---
+++ index.js
@@ -0,0 +1,88 @@
+/**
+ * Created by yicj on 2016/3/25.
+ */
+"use strict"
+function testLet(){
+    for(let i = 0 ; i < 10 ;i++){
+
+    }
+    //这里无法访问到变量 let
+    console.info(i) ;
+}
+
+function testVar(){
+    for(var i = 0 ; i < 10 ;i++){
+    }
+    //这里可以访问到变量 let
+    console.info(i) ;
+}
+//testLet() ;
+//testVar() ;
+
+function testVar2(){
+    var a = [] ;
+    for(var i = 0 ; i < 10 ;i ++){
+        a[i] = function(){
+            console.info(i) ;
+        }
+    }
+    a[6]() ;
+}
+
+//testVar2() ;
+function testLet2(){
+    var a = [] ;
+    for(let i = 0 ; i < 10 ; i++){
+        a[i] = function () {
+            console.info(i) ;
+        }
+    }
+    a[6]() ;
+}
+//testLet2() ;
+//
+function test3(){
+    var tmp = 123 ;
+    if(true){
+        //tmp = 'abc' ;
+        console.info(tmp) ;
+        let tmp ;
+    }
+}
+
+//test3() ;
+var tmp = new Date() ;
+function test4(){
+    console.info(tmp) ;
+    if(false){
+        var tmp = "hello world" ;
+    }
+}
+
+//test4() ;
+
+
+{
+    let  a = 11 ;
+    console.info(a) ;
+}
+
+{
+    let a = 'secret' ;
+    function f(){
+        return a ;
+    }
+}
+//f() ;
+
+{
+
+    var [a, b, c] = [1, 2, 3];
+    console.info(a) ;
+    console.info(b) ;
+    console.info(c) ;
+
+}
+
+
+
