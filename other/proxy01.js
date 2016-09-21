81c47e46021e4f845b7b4daa6f91a689fa61fb41
yichengjie <626659321@qq.com>
2016-09-21 22:16:45 +0800

    commit

---
+++ other/proxy01.js
@@ -0,0 +1,21 @@
+'use strict'
+var Reflect = require('harmony-reflect');
+//import 'babel-polyfill';
+
+var obj = new Proxy({}, {
+  get: function (target, key, receiver) {
+    console.log(`getting ${key}!`);
+    return Reflect.get(target, key, receiver);
+  },
+  set: function (target, key, value, receiver) {
+    console.log(`setting ${key}!`);
+    return Reflect.set(target, key, value, receiver);
+  }
+});
+
+obj.count = 1
+//  setting count!
+++obj.count
+//  getting count!
+//  setting count!
+//  2
